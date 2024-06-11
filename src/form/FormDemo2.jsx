6918ca88727144e6463be3f3ee0b15ff8d6400e0
samirvithlani <samir.vithlani83955@gmail.com>
2024-06-11 09:27:09 +0530

    task

--- src/form/FormDemo2.jsx
+++ src/form/FormDemo2.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
 export const FormDemo2 = () => {
   //useForm
 
   const { register, handleSubmit } = useForm();
+  const [output, setoutput] = useState({});
+  const [usercolor, setcolor] = useState("black");
+  const [isFormSubmited, setisFormSubmited] = useState(false);
+
   //register is used to register the input fields, it is an object
   //handleSubmit is used to submit the form, it is a function
   const submitHandler = (data) => {
     console.log("submit handler...", data);
+    setoutput(data);
+    setcolor(data.color);
+    setisFormSubmited(true);
   };
 
   return (
@@ -28,20 +35,73 @@ export const FormDemo2 = () => {
           <input type="text" {...register("age")}></input>
         </div>
         <div>
-            <label>GENDER</label>
-            <br/> MALE :<input type="radio" value="male" name="gender" {...register("gender")}></input>
-            <br/> FEMALE :<input type="radio" value="female" name="gender" {...register("gender")}></input>
+          <label>GENDER</label>
+          <br /> MALE :
+          <input
+            type="radio"
+            value="male"
+            name="gender"
+            {...register("gender")}
+          ></input>
+          <br /> FEMALE :
+          <input
+            type="radio"
+            value="female"
+            name="gender"
+            {...register("gender")}
+          ></input>
         </div>
         <div>
-            <label>SKILLS</label>
-            <br/> JAVA :<input type="checkbox" value="java" name="skills" {...register("skills")}></input>
-            <br/> REACT :<input type="checkbox" value="react" name="skills" {...register("skills")}></input>
-            <br/> ANGULAR :<input type="checkbox" value="angular" name="skills" {...register("skills")}></input>
+          <label>SKILLS</label>
+          <br /> JAVA :
+          <input
+            type="checkbox"
+            value="java"
+            name="skills"
+            {...register("skills")}
+          ></input>
+          <br /> REACT :
+          <input
+            type="checkbox"
+            value="react"
+            name="skills"
+            {...register("skills")}
+          ></input>
+          <br /> ANGULAR :
+          <input
+            type="checkbox"
+            value="angular"
+            name="skills"
+            {...register("skills")}
+          ></input>
+        </div>
+        <div>
+          <label>COLOR</label>
+          <input type="color" {...register("color")}></input>
+        </div>
+        <div>
+          <label>SELECT Country</label>
+          <select {...register("country")}>
+            <option value="select">SELECT</option>
+            <option value="india">INDIA</option>
+            <option value="usa">USA</option>
+            <option value="uk">UK</option>
+          </select>
         </div>
         <div>
           <input type="submit" value="Submit"></input>
         </div>
       </form>
+      {isFormSubmited && (
+        <div style={{ color: usercolor }}>
+          <h2>Output</h2>
+          <p>First Name : {output.firstName}</p>
+          <p>Last Name : {output.lastName}</p>
+          <p>Age : {output.age}</p>
+          <p>GENDER :{output.gender}</p>
+          <p>COUNTRY : {output.country}</p>
+        </div>
+      )}
     </div>
   );
 };
