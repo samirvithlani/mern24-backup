7df1ac8e9f8b41465f1a86f382a636ed92576268
samirvithlani <samir.vithlani83955@gmail.com>
2024-06-13 09:32:04 +0530

    form validation

--- src/form/FormDemo3.jsx
+++ src/form/FormDemo3.jsx
@@ -39,6 +39,32 @@ export const FormDemo3 = () => {
             value:10,
             message:"address should be less than 10"
         }
+    },
+    phone:{
+        required:{
+            value:true,
+            message:"phone is required*"
+        },
+        pattern:{
+            value:/^[6-9]{1}[0-9]{9}$/,
+            message:"invalid phone number"
+        }
+    },
+    gender:{
+        required:{
+            value:true,
+            message:"Gender is required*"
+        }
+    },
+    email:{
+        required:{
+            value:true,
+            message:"email is required*"
+        },
+        pattern:{
+            value:/^[a-zA-Z0-9._%+-]+@gmail\.com$/,
+            message:"invalid email"
+        }
     }
   };
 
@@ -71,9 +97,36 @@ export const FormDemo3 = () => {
             }
         </div>
         <div>
+            <label>PHONE</label>
+            <input type="text" {...register("phone",validationSchema.phone)}></input>
+            {
+                errors.phone && <span>{errors.phone.message}</span>
+            }
+        </div>
+        <div>
+            <label>GENDER</label>
+            MALE : <input type="radio" name="gender" value="male" {...register("gender",validationSchema.gender)}></input>
+            FEMALE : <input type="radio" name="gender" value="female" {...register("gender",validationSchema.gender)}></input>
+            {
+
+                errors.gender && <span>{errors.gender.message}</span>
+            }
+        </div>
+        <div>
+            <label>EMAIL</label>
+            <input type="text" {...register("email",validationSchema.email)}></input>
+            {
+                errors.email && <span>{errors.email.message}</span>
+            
+            }
+        </div>
+        
+        <div>
           <input type="submit" value="submit"></input>
         </div>
       </form>
+
+      
     </div>
   );
 };
